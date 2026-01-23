a891e4605ed9cded81c67983d06a205c1a87723f
cindybhuynh <chbmv@umsystem.edu>
2026-01-23 11:29:08 -0600

    Add Hello World app and screenshot

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Cindy Huynh on 1/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
